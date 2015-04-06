4c490d904648f0f1737f3cdb759516058f4e4f12
christophecvr <stefansat@telenet.be>
2015-04-06 16:22:50 +0200

    Limit max rate to 48000 by dreambox.  Dreambox pcm does not support rate 96000.  But plays well the same audio if converted to 48000 rate.  Changes to be committed: 	modified:   gstdvbaudiosink.c

--- gstdvbaudiosink.c
+++ gstdvbaudiosink.c
@@ -142,11 +142,19 @@ static guint gst_dvbaudiosink_signals[LAST_SIGNAL] = { 0 };
 		"rate = (int) {8000, 16000}, channels = (int) 1; "
 
 #define XRAW "audio/x-raw"
+#ifdef DREAMBOX
+#define PCMCAPS \
+		"audio/x-raw, " \
+		"format = (string) { "GST_AUDIO_NE(S32)", "GST_AUDIO_NE(S24)", "GST_AUDIO_NE(S16)", S8, "GST_AUDIO_NE(U32)", "GST_AUDIO_NE(U24)", "GST_AUDIO_NE(U16)", U8 }, " \
+		"layout = (string) { interleaved, non-interleaved }, " \
+		"rate = (int) [ 1, 48000 ], " "channels = (int) [ 1, 2 ]; "
+#else
 #define PCMCAPS \
 		"audio/x-raw, " \
 		"format = (string) { "GST_AUDIO_NE(S32)", "GST_AUDIO_NE(S24)", "GST_AUDIO_NE(S16)", S8, "GST_AUDIO_NE(U32)", "GST_AUDIO_NE(U24)", "GST_AUDIO_NE(U16)", U8 }, " \
 		"layout = (string) { interleaved, non-interleaved }, " \
 		"rate = (int) [ 1, MAX ], " "channels = (int) [ 1, 2 ]; "
+#endif
 
 static GstStaticPadTemplate sink_factory =
 GST_STATIC_PAD_TEMPLATE(
